781491e21a7f3a245b87c4da38dcaf40f9101733
Velascouu <diego.velper.2@gmail.com>
2021-10-07 09:29:50 +0200

    rtfdrf

--- TareaJueves07/js/funciones.js
+++ TareaJueves07/js/funciones.js
@@ -1,21 +1,32 @@
 
-cargarXML();
-
+//Guardamos en una variable los elementos
 var iISBN = document.getElementById("iISBN");
+var titulo = document.getElementsById("Titulo");
+var autor = document.getElementsById("Autor");
+var editorial = document.getElementsById("Editorial");
+var paginas = document.getElementsById("Paginas");
+
+//Creamos los Arrays para guardar los datos
+var aISBN = new Array();
+var aTitulo = new Array();
+var aAutor = new Array();
+var aEditorial = new Array();
+var aPaginas = new Array();
+
+var posicion = 0;
+
+
+
+bSiguiente.addEventListenner("click", registroSiguiente, false);
 
 function cargarXML(){
     // En esta funcion leemos los datos del fichero datos.js 
     // en formato xml y lo transformamos en una coleccion de arrays
 
+    alert("sbdvbs");
+
     var codigo=new DOMParser();
     var myXml = codigo.parseFromString(datosFichero,"text/xml");
-    alert(myXml);
-
-    var aISBN = new Array();
-    var aTitulo = new Array();
-    var aAutor = new Array();
-    var aEditorial = new Array();
-    var aPaginas = new Array();
 
     aISBN = myXml.getElementsByTagName("isbn");
     aTitulo = myXml.getElementsByTagName("titulo");
@@ -25,6 +36,25 @@ function cargarXML(){
 
     //alert(aISBN[0].firstChild.nodeValue);
 
-    iISBN.nodeValue = aISBN[0].firstChild.nodeValue;
+    alert(aISBN[0].firstChild.nodeValue);
+
+    mostrarRegistro(posicion);
 
 }
+
+function registroSiguiente(){
+    posicion++;
+    mostrarRegistro(posicion);
+}
+
+function mostrarRegistro(posicion){
+
+    iISBN.value = aISBN[posicion].firstChild.nodeValue;
+    titulo.value = aTitulo[posicion].firstChild.nodeValue;
+    autor.value = aAutor[posicion].firstChild.nodeValue;
+    editorial.value = aEditorial[posicion].firstChild.nodeValue;
+    paginas.value = aPaginas[posicion].firstChild.nodeValue;
+
+}
+
+cargarXML();
